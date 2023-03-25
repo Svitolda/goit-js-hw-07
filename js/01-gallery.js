d8a53b98bfb2559acda7571bbbfa98a2bf437c1e
Svitolda <sveta20000203@gmail.com>
2023-03-25 21:48:48 +0100

    Task1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,58 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryListReference = document.querySelector('.gallery');
+const imageMarkup = createGalleryImageMarkup(galleryItems);
+
+// console.log(galleryListReference)
+galleryListReference.insertAdjacentHTML('beforeend', imageMarkup);
+
+galleryListReference.addEventListener('click', ongalleryClick);
+
+
+function createGalleryImageMarkup(galleryItems) {
+return galleryItems
+    .map((image) =>
+    `<li li class="gallery__item">
+        <a class="gallery__link" href= ${image.original} >
+            <img
+                class="gallery__image"
+                src= ${image.preview}
+                data-source= ${image.original}
+                alt= ${image.description}
+            />
+        </a>
+    </li>`).join("");
+};
+
+function ongalleryClick(evt) {
+    evt.preventDefault();
+
+    const currentItem = evt.target;
+    if (currentItem.nodeName !== 'IMG') {
+        return;
+    }
+    // console.log(evt.target);
+
+    const instance = basicLightbox.create(`
+        <img src="${evt.target.dataset.source}" width="800" height="600">
+    `);
+    
+    // {
+    //     onShow: (instance) => {
+    //         galleryListReference.addEventListener("keydown", (event) => {
+    //             if (event.code === 'Escape') {
+    //                 instance.close();
+    //             }
+    //         })
+    //     },
+    //         onClose: (instance) => {
+    //             galleryListReference.removeEventListener("keydown", (event) => {
+    //                 if (event.code === 'Escape') {
+    //                     instance.close();
+    //                 }
+    //             })
+    //         }
+        instance.show()
+}
