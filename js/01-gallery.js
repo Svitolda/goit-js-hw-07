18fe99a13a5a88917aca4042a7667da5071b9716
Svitolda <sveta20000203@gmail.com>
2023-03-25 22:44:44 +0100

    task1update

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -16,12 +16,12 @@ function createGalleryImageMarkup(galleryItems) {
 return galleryItems
     .map((image) =>
     `<li li class="gallery__item">
-        <a class="gallery__link" href= ${image.original} >
+        <a class="gallery__link" href= "${image.original}" >
             <img
                 class="gallery__image"
-                src= ${image.preview}
-                data-source= ${image.original}
-                alt= ${image.description}
+                src= "${image.preview}"
+                data-source= "${image.original}"
+                alt= "${image.description}"
             />
         </a>
     </li>`).join("");
@@ -29,31 +29,13 @@ return galleryItems
 
 function ongalleryClick(evt) {
     evt.preventDefault();
-
     const currentItem = evt.target;
     if (currentItem.nodeName !== 'IMG') {
         return;
     }
-    // console.log(evt.target);
-
     const instance = basicLightbox.create(`
         <img src="${evt.target.dataset.source}" width="800" height="600">
     `);
+    instance.show()
     
-    // {
-    //     onShow: (instance) => {
-    //         galleryListReference.addEventListener("keydown", (event) => {
-    //             if (event.code === 'Escape') {
-    //                 instance.close();
-    //             }
-    //         })
-    //     },
-    //         onClose: (instance) => {
-    //             galleryListReference.removeEventListener("keydown", (event) => {
-    //                 if (event.code === 'Escape') {
-    //                     instance.close();
-    //                 }
-    //             })
-    //         }
-        instance.show()
 }
